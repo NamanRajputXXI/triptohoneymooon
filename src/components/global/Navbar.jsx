aa72bdb680cffb9aea545984eb433f259eff767f
Naman Rajput <namanrajput801@gmail.com>
2024-05-21 18:13:42 +0530

    ui:changes in the navbar

--- src/components/global/Navbar.jsx
+++ src/components/global/Navbar.jsx
@@ -129,6 +129,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
+import { RxCross1 } from "react-icons/rx";
 import Button from "./Button";
 
 const Navbar = () => {
@@ -173,21 +174,7 @@ const Navbar = () => {
               onClick={toggleNavbar}
             >
               {isOpen ? (
-                <svg
-                  className="hs-collapse-open:block hidden flex-shrink-0 size-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M18 6 6 18" />
-                  <path d="m6 6 12 12" />
-                </svg>
+                <RxCross1 className="hs-collapse-open:block " size={20} />
               ) : (
                 <svg
                   className="hs-collapse-open:hidden flex-shrink-0 size-4"
