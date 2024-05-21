74ba579de8af801e2e4f3651de8fc7a1d92cff90
Naman Rajput <namanrajput801@gmail.com>
2024-05-21 17:56:46 +0530

    ui:changes in the home page

--- src/components/home/OurServices.jsx
+++ src/components/home/OurServices.jsx
@@ -1,11 +1,75 @@
+// import React from "react";
+
+// const OurServices = () => {
+//   return (
+//     <section className="bg-red-100 mb-3 h-fit  md:pt-20">
+//       <div className="max-w-7xl  px-5 mx-auto">
+//         <h1 className=" md:text-5xl text-2xl mb-10">Our Services</h1>
+//         <div className="grid h-64 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:grid-cols-4 justify-center items-center gap-5 w-full">
+//           <div className="">
+//             <div className="flex flex-shrink-0 relative w-full sm:w-auto">
+//               <img
+//                 src="https://images.unsplash.com/photo-1571896349842-33c89424de2d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG90ZWxzfGVufDB8fDB8fHww"
+//                 alt=""
+//                 className="object-cover rounded-2xl object-center h-80 w-full md:w-96"
+//               />
+//               <div className="text-xl lg:text-2xl text-center rounded-2xl flex items-end justify-start font-semibold absolute h-full bg-black bg-opacity-30 w-full leading-5 lg:leading-7 text-white">
+//                 <h1 className=" text-white mx-4 my-4">Hotels</h1>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="">
+//             <div className="flex flex-shrink-0 relative w-full sm:w-auto">
+//               <img
+//                 src="https://images.unsplash.com/photo-1542992804-34f8f4cb193b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cGhvdG9zaG9vdHxlbnwwfDB8MHx8fDA%3D"
+//                 alt=""
+//                 className="object-cover rounded-2xl object-center h-80 w-full md:w-96"
+//               />
+//               <div className="text-xl lg:text-2xl text-center rounded-2xl flex items-end justify-start font-semibold absolute h-full bg-black bg-opacity-30 w-full leading-5 lg:leading-7 text-white">
+//                 <h1 className=" text-white mx-4 my-4">Photoshoot</h1>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="">
+//             <div className="flex flex-shrink-0 relative w-full sm:w-auto">
+//               <img
+//                 src="https://images.unsplash.com/photo-1546032996-6dfacbacbf3f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8d2VkZGluZ3xlbnwwfDF8MHx8fDA%3D"
+//                 alt=""
+//                 className="object-cover rounded-2xl object-center h-80 w-full md:w-96"
+//               />
+//               <div className="text-xl lg:text-2xl text-center rounded-2xl flex items-end justify-start font-semibold absolute h-full bg-black bg-opacity-30 w-full leading-5 lg:leading-7 text-white">
+//                 <h1 className=" text-white mx-4 my-4">Wedding</h1>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="">
+//             <div className="flex flex-shrink-0 relative w-full sm:w-auto">
+//               <img
+//                 src="https://images.unsplash.com/photo-1544928147-79a2dbc1f389?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZXZlbnR8ZW58MHwxfDB8fHww"
+//                 alt=""
+//                 className="object-cover rounded-2xl object-center h-80 w-full md:w-96"
+//               />
+//               <div className="text-xl lg:text-2xl text-center  flex rounded-2xl items-end justify-start font-semibold absolute h-full bg-black bg-opacity-30 w-full leading-5 lg:leading-7 text-white">
+//                 <h1 className=" text-white mx-4 my-4">Event</h1>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default OurServices;
+
 import React from "react";
 
 const OurServices = () => {
   return (
-    <section className="bg-red-100 mb-3 h-fit  md:pt-20">
-      <div className="max-w-7xl  px-5 mx-auto">
+    <section className="bg-red-100 mb-20 h-fit md:pt-20">
+      <div className="max-w-7xl px-5 mx-auto">
         <h1 className=" md:text-5xl text-2xl mb-10">Our Services</h1>
-        <div className="grid h-64 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:grid-cols-4 justify-center items-center gap-5 w-full">
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:grid-cols-4 justify-center items-center gap-5 w-full h-auto">
           <div className="">
             <div className="flex flex-shrink-0 relative w-full sm:w-auto">
               <img
@@ -49,7 +113,7 @@ const OurServices = () => {
                 alt=""
                 className="object-cover rounded-2xl object-center h-80 w-full md:w-96"
               />
-              <div className="text-xl lg:text-2xl text-center  flex rounded-2xl items-end justify-start font-semibold absolute h-full bg-black bg-opacity-30 w-full leading-5 lg:leading-7 text-white">
+              <div className="text-xl lg:text-2xl text-center flex rounded-2xl items-end justify-start font-semibold absolute h-full bg-black bg-opacity-30 w-full leading-5 lg:leading-7 text-white">
                 <h1 className=" text-white mx-4 my-4">Event</h1>
               </div>
             </div>
