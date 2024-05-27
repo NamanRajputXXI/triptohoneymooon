ed60e12847bb6c4258db62f4d0f582de87cb8e99
Naman Rajput <namanrajput801@gmail.com>
2024-05-27 10:24:28 +0530

    ui:changes in footer and domestic destination

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -4,56 +4,77 @@ import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="relative md:mt-20 mt-10 h-fit py-20 w-full bg-[url('https://images.unsplash.com/photo-1497120573086-6219573cf71c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzV8fGhvbmV5bW9vbnxlbnwwfDB8MHx8fDA%3D')] bg-cover bg-center">
+    <footer className="relative md:mt-20 mt-10 h-fit  md:py-20 w-full bg-red-900">
       <div className="absolute h-full w-full  inset-0 bg-black bg-opacity-60"></div>
-      <div className="grid max-w-7xl gap-8 mx-auto relative z-10 font-medium  lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 items-start text-white justify-center p-10">
-        <div className="flex flex-col lg:items-start lg:justify-start items-center justify-center gap-8">
-          <h1 className="text-2xl font-semibold">TRIP TO HONEYMOON</h1>
-          <p className="">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita,
-            esse. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel,
-            inventore!
-          </p>
-        </div>
-        <div className="flex flex-col  items-center lg:pl-20 gap-5">
-          <h1 className="text-xl font-semibold">Policy</h1>
-          <div className="flex flex-col gap-1">
-            <p>Privacy Policy</p>
-            <p>Terms of Use</p>
+      <div className="grid max-w-7xl gap-8 mx-auto relative z-10 font-medium  lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 items-start text-white justify-center p-10">
+        <div className="flex flex-col gap-10">
+          <div className="flex flex-col gap-3">
+            <h1 className="text-xl font-semibold">Policy</h1>
+            <div className="flex flex-col gap-3">
+              <p>Privacy Policy</p>
+              <p>Terms of Use</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <h1 className="text-xl font-semibold">Support</h1>
+            <div className="flex flex-col gap-3">
+              <p>Contact Us</p>
+              <p>About Us</p>
+            </div>
           </div>
         </div>
-        <div className="flex flex-col items-center  lg:pl-20  gap-5">
-          <h1 className="text-xl font-semibold">Support</h1>
-          <div className="flex flex-col gap-1">
-            <p>Privacy Policy</p>
-            <p>Terms of Use</p>
+        <div className="flex flex-col  gap-10">
+          <div className="flex flex-col    gap-3">
+            <h1 className="text-xl font-semibold">Quick Link</h1>
+            <div className="flex flex-col gap-3">
+              <p>Home</p>
+              <p>Trending Destination</p>
+              <p>Domestic Destination</p>
+              <p>Popular Destination</p>
+              <p>International Destination </p>
+            </div>
           </div>
         </div>
-        <div className="flex flex-col items-center  lg:pl-20  gap-5">
-          <h1 className="text-xl  font-semibold">Contact Us</h1>
-          <div className="flex flex-col gap-1">
-            <p>Privacy Policy</p>
-            <p>Terms of Use</p>
+        <div className="flex flex-col    gap-10">
+          <div className="flex flex-col gap-3">
+            <h1 className="text-xl  font-semibold">Follow Us on</h1>
+            <div className="flex items-center gap-3">
+              <Link href={"ab"}>
+                <FaFacebook size={30} color="#1877F2" />
+              </Link>
+              <Link href={"ab"}>
+                <FaInstagram size={30} color="#FCAF45" />
+              </Link>
+              <Link href={"ab"}>
+                <FaYoutube size={30} color="#CD201F" />
+              </Link>
+            </div>
+          </div>
+          <div className="flex  flex-col gap-3">
+            <h1 className="text-xl  font-semibold">Sign Up</h1>
+            <div className="flex  flex-col gap-3">
+              <p className="">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae
+                omnis, a cum amet sed officia dolorem atque quibusdam
+                dignissimos cupiditate.
+              </p>
+              <div className="flex items-center gap-2">
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="px-5 py-2 placeholder:text-white bg-gray-400 text-white flex items-center  rounded-xl"
+                />
+                <button className="flex items-center justify-center px-5 py-2 text-black bg-white rounded-xl">
+                  send
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <hr className="relative mx-10  my-10 z-10 h-1" />
-      <div className="max-w-7xl  md:flex-row flex-col gap-5 px-5 relative z-10 text-white mx-auto flex items-center justify-between">
+
+      <div className="max-w-7xl md:pb-0 pb-10 mt-5 md:flex-row flex-col gap-5 px-5 relative z-10 text-gray-400 mx-auto flex items-center justify-between">
         <p>@Triptohoneymoon 2024 All Rights Reserved</p>
-        <div className="flex items-center gap-3">
-          <p>Follow us on</p>
-          <div className="flex items-center gap-3">
-            <Link href={"ab"}>
-              <FaFacebook size={30} />
-            </Link>
-            <Link href={"ab"}>
-              <FaInstagram size={30} color="pink" />
-            </Link>
-            <Link href={"ab"}>
-              <FaYoutube size={30} color="red" />
-            </Link>
-          </div>
-        </div>
       </div>
     </footer>
   );
