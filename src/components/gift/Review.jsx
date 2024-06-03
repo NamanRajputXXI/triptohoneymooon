ec0fb915d213f5a9edbcd9079e9f3640888e3d28
Naman Rajput <namanrajput801@gmail.com>
2024-06-03 15:32:46 +0530

    ui:changes in review section in gift an exp page

--- src/components/gift/Review.jsx
+++ src/components/gift/Review.jsx
@@ -5,13 +5,13 @@ const Review = () => {
     <section className="md:py-20 px-5 py-10">
       <div className="max-w-7xl md:flex-row flex-col   items-center bg-purple-950 rounded-2xl mx-auto flex   md:p-10 p-5">
         <div className="flex-col gap-5 flex justify-center items-center  py-5">
-          <p className="md:text-5xl text-white text-center sm:text-xl  text-base font-bold">
+          <p className="md:text-5xl text-white text-center sm:text-xl px-2   text-sm font-bold">
             “I love how easy it is to order and it makes everyone so happy.”
           </p>
-          <p className="text-gray-300 sm:text-start text-center  font-bold flex items-center text-bold">
+          <p className="text-gray-300 sm:text-base text-sm sm:text-start text-center  font-bold flex items-center text-bold">
             Gayle, Google Reviewer
           </p>
-          <div className="flex text-white text-3xl font-bold gap-5 items-center">
+          <div className="flex sm:flex-row flex-col sm:justify-start justify-center text-white text-xl sm:text-3xl font-bold gap-5 items-center">
             4.9
             <div className="flex gap-2 items-center">
               <FaStar color="#eab308" size={30} />
@@ -19,7 +19,9 @@ const Review = () => {
               <FaStar color="#eab308" size={30} />
               <FaStar color="#eab308" size={30} />
               <FaStar color="#eab308" size={30} />
-              <p className="font-medium text-base">(1,089 Google Reviews)</p>
+              <p className="font-medium text-sm sm:text-base">
+                (1,089 Google Reviews)
+              </p>
             </div>
           </div>
         </div>
--- src/components/home/HomePageCarousel/AttractivePlannerCarousel.jsx
+++ src/components/home/HomePageCarousel/AttractivePlannerCarousel.jsx
@@ -93,7 +93,7 @@ const AttractivePlannerCarousel = () => {
             <button
               aria-label="Previous slide"
               onClick={handlePrevSlide}
-              className="rounded-full border flex justify-center  items-center bg-gray-500 p-3 text-rose-600 transition  hover:text-white"
+              className="rounded-full border flex justify-center  items-center bg-gray-400 p-3 text-rose-600 transition  hover:text-white"
             >
               <span className="inline-block h-5 w-5">
                 <svg
@@ -112,7 +112,7 @@ const AttractivePlannerCarousel = () => {
             <button
               aria-label="Next slide"
               onClick={handleNextSlide}
-              className="rounded-full  bg-gray-500  p-3 flex justify-center items-center text-rose-600 transition  hover:text-white"
+              className="rounded-full  bg-gray-400  p-3 flex justify-center items-center text-rose-600 transition  hover:text-white"
             >
               <span className="inline-block h-5 w-5">
                 <svg
