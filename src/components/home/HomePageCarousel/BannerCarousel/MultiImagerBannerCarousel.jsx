2ffcce0b295fe6d8312fc624e5ef7d77d8fd1d12
Naman Rajput <namanrajput801@gmail.com>
2024-06-08 12:22:27 +0530

    ui:changes in the multiimage banner carousel

--- src/components/home/HomePageCarousel/BannerCarousel/MultiImagerBannerCarousel.jsx
+++ src/components/home/HomePageCarousel/BannerCarousel/MultiImagerBannerCarousel.jsx
@@ -114,9 +114,9 @@ const MultiImagerBannerCarousel = () => {
   };
 
   return (
-    <div className="flex my-20 px-5 justify-end h-64 items-center max-w-7xl mx-auto gap-3">
-      <div className="flex image-carousel w-1/2 h-64 ">
-        <div className="overflow-hidden rounded-xl h-64 relative">
+    <div className="flex lg:flex-row flex-col  my-20 px-5 justify-end h-fit lg:h-64 items-center max-w-7xl mx-auto gap-3">
+      <div className="flex image-carousel w-full lg:w-1/2 h-48 sm:h-64 ">
+        <div className="overflow-hidden rounded-xl h-48 sm:h-64 relative">
           <div
             className={`flex transition ease-out duration-500`}
             style={{ transform: `translateX(-${current * 100}%)` }}
@@ -150,24 +150,24 @@ const MultiImagerBannerCarousel = () => {
           </div>
         </div>
       </div>
-      <div className="flex items-center gap-3  w-1/2">
-        <div className="flex w-1/3 h-64 rounded-xl overflow-hidden">
+      <div className="flex items-center gap-3 w-full  lg:w-1/2">
+        <div className="flex w-1/3 h-44 sm:h-64 rounded-xl overflow-hidden">
           <img
-            className="rounded-xl w-full transition-all duration-500 hover:scale-110"
+            className="rounded-xl w-full object-cover object-center transition-all duration-500 hover:scale-110"
             src="https://plus.unsplash.com/premium_photo-1664541337081-421a05d7e6ec?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y291cGxlfGVufDB8MXwwfHx8MA%3D%3D"
             alt=""
           />
         </div>
-        <div className="flex w-1/3 h-64 rounded-xl overflow-hidden">
+        <div className="flex w-1/3 h-44 sm:h-64 rounded-xl overflow-hidden">
           <img
-            className="rounded-xl w-full transition-all duration-500 hover:scale-110"
+            className="rounded-xl w-full object-cover object-center transition-all duration-500 hover:scale-110"
             src="https://images.unsplash.com/photo-1481841580057-e2b9927a05c6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y291cGxlfGVufDB8MXwwfHx8MA%3D%3D"
             alt=""
           />
         </div>
-        <div className="flex w-1/3   h-64 rounded-xl overflow-hidden">
+        <div className="flex w-1/3  h-44 sm:h-64 rounded-xl overflow-hidden">
           <img
-            className="rounded-xl transition-all w-full duration-500 hover:scale-110"
+            className="rounded-xl object-cover object-center transition-all w-full duration-500 hover:scale-110"
             src="https://images.unsplash.com/photo-1591969851586-adbbd4accf81?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvdXBsZXxlbnwwfDF8MHx8fDA%3D"
             alt=""
           />
