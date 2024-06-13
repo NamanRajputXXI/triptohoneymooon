a9f297a7d09afddc91607e1d4544d79151b4442b
Naman Rajput <namanrajput801@gmail.com>
2024-06-13 15:48:38 +0530

    feat:add lazy loading on the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,85 @@
 // "use client";
-// import DestinationList from "@/components/global/DestinationList";
-// import Footer from "@/components/global/Footer";
+
 // import Navbar from "@/components/global/Navbar";
-// import AboutUsSection from "@/components/home/AboutUsSection";
-// import AdsCarousel from "@/components/home/AdsCarousel";
-// import BrandingSidebar from "@/components/home/BrandingSidebar";
-// import DomesticDestinationSection from "@/components/home/DomesticDestination";
 // import HeroSection from "@/components/home/HeroSection";
-// import AttractivePlannerCarousel from "@/components/home/HomePageCarousel/AttractivePlannerCarousel";
-// import BannerCarousel from "@/components/home/HomePageCarousel/BannerCarousel/BannerCarousel";
-// import MultiImagerBannerCarousel from "@/components/home/HomePageCarousel/BannerCarousel/MultiImagerBannerCarousel";
 // import WeddingBannerCarousel from "@/components/home/HomePageCarousel/BannerCarousel/WeddingBannerCarousel";
 // import HeroSectionCarousel from "@/components/home/HomePageCarousel/HeroSectionCarousel";
-// import BestSellingCarousel from "@/components/home/HomePageCarousel/ProductCarousel/BestSellingCarousel";
-// import DomesticCarousel from "@/components/home/HomePageCarousel/ProductCarousel/DomesticCarousel";
-// import InternationalCarousel from "@/components/home/HomePageCarousel/ProductCarousel/InternationalCarousel";
-// import PopularCarousel from "@/components/home/HomePageCarousel/ProductCarousel/PopularCarousel";
-// import TrendingCarousel from "@/components/home/HomePageCarousel/ProductCarousel/TrendingCarousel";
-// import InternationDestinationSection from "@/components/home/InternationDestinationSection";
-// import OurResorts from "@/components/home/OurResorts";
-// import OurServices from "@/components/home/OurServices";
 // import PopularDestinationSection from "@/components/home/PopularDestinationSection";
+// import dynamic from "next/dynamic";
+
+// const TrendingCarousel = dynamic(
+//   () =>
+//     import(
+//       "@/components/home/HomePageCarousel/ProductCarousel/TrendingCarousel"
+//     ),
+//   { ssr: false }
+// );
+// const BestSellingCarousel = dynamic(
+//   () =>
+//     import(
+//       "@/components/home/HomePageCarousel/ProductCarousel/BestSellingCarousel"
+//     ),
+//   { ssr: false }
+// );
+// const DomesticCarousel = dynamic(
+//   () =>
+//     import(
+//       "@/components/home/HomePageCarousel/ProductCarousel/DomesticCarousel"
+//     ),
+//   { ssr: false }
+// );
+// const InternationalCarousel = dynamic(
+//   () =>
+//     import(
+//       "@/components/home/HomePageCarousel/ProductCarousel/InternationalCarousel"
+//     ),
+//   { ssr: false }
+// );
+// const AttractivePlannerCarousel = dynamic(
+//   () => import("@/components/home/HomePageCarousel/AttractivePlannerCarousel"),
+//   { ssr: false }
+// );
+// const AboutUsSection = dynamic(
+//   () => import("@/components/home/AboutUsSection"),
+//   { ssr: false }
+// );
+// const PopularCarousel = dynamic(
+//   () =>
+//     import(
+//       "@/components/home/HomePageCarousel/ProductCarousel/PopularCarousel"
+//     ),
+//   { ssr: false }
+// );
+// const MultiImagerBannerCarousel = dynamic(
+//   () =>
+//     import(
+//       "@/components/home/HomePageCarousel/BannerCarousel/MultiImagerBannerCarousel"
+//     ),
+//   { ssr: false }
+// );
+// const BannerCarousel = dynamic(
+//   () =>
+//     import("@/components/home/HomePageCarousel/BannerCarousel/BannerCarousel"),
+//   { ssr: false }
+// );
+// const InternationDestinationSection = dynamic(
+//   () => import("@/components/home/InternationDestinationSection"),
+//   { ssr: false }
+// );
+
+// const DomesticDestinationSection = dynamic(
+//   () => import("@/components/home/DomesticDestination"),
+//   { ssr: false }
+// );
+// const OurResorts = dynamic(() => import("@/components/home/OurResorts"), {
+//   ssr: false,
+// });
+// const OurServices = dynamic(() => import("@/components/home/OurServices"), {
+//   ssr: false,
+// });
+// const Footer = dynamic(() => import("@/components/global/Footer"), {
+//   ssr: false,
+// });
 
 // export default function Home() {
 //   return (
@@ -50,101 +109,57 @@
 
 "use client";
 
-import dynamic from "next/dynamic";
+import Navbar from "@/components/global/Navbar";
+import HeroSection from "@/components/home/HeroSection";
+import WeddingBannerCarousel from "@/components/home/HomePageCarousel/BannerCarousel/WeddingBannerCarousel";
+import HeroSectionCarousel from "@/components/home/HomePageCarousel/HeroSectionCarousel";
+import React, { lazy, Suspense } from "react";
+import { InView } from "react-intersection-observer";
 
-const Navbar = dynamic(() => import("@/components/global/Navbar"), {
-  ssr: false,
-});
-const WeddingBannerCarousel = dynamic(
-  () =>
-    import(
-      "@/components/home/HomePageCarousel/BannerCarousel/WeddingBannerCarousel"
-    ),
-  { ssr: false }
+// Lazy loaded components
+const TrendingCarousel = lazy(() =>
+  import("@/components/home/HomePageCarousel/ProductCarousel/TrendingCarousel")
 );
-const HeroSection = dynamic(() => import("@/components/home/HeroSection"), {
-  ssr: false,
-});
-const HeroSectionCarousel = dynamic(
-  () => import("@/components/home/HomePageCarousel/HeroSectionCarousel"),
-  { ssr: false }
+const BestSellingCarousel = lazy(() =>
+  import(
+    "@/components/home/HomePageCarousel/ProductCarousel/BestSellingCarousel"
+  )
 );
-const TrendingCarousel = dynamic(
-  () =>
-    import(
-      "@/components/home/HomePageCarousel/ProductCarousel/TrendingCarousel"
-    ),
-  { ssr: false }
+const DomesticCarousel = lazy(() =>
+  import("@/components/home/HomePageCarousel/ProductCarousel/DomesticCarousel")
 );
-const BestSellingCarousel = dynamic(
-  () =>
-    import(
-      "@/components/home/HomePageCarousel/ProductCarousel/BestSellingCarousel"
-    ),
-  { ssr: false }
+const InternationalCarousel = lazy(() =>
+  import(
+    "@/components/home/HomePageCarousel/ProductCarousel/InternationalCarousel"
+  )
 );
-const DomesticCarousel = dynamic(
-  () =>
-    import(
-      "@/components/home/HomePageCarousel/ProductCarousel/DomesticCarousel"
-    ),
-  { ssr: false }
+const AttractivePlannerCarousel = lazy(() =>
+  import("@/components/home/HomePageCarousel/AttractivePlannerCarousel")
 );
-const InternationalCarousel = dynamic(
-  () =>
-    import(
-      "@/components/home/HomePageCarousel/ProductCarousel/InternationalCarousel"
-    ),
-  { ssr: false }
+const AboutUsSection = lazy(() => import("@/components/home/AboutUsSection"));
+const PopularCarousel = lazy(() =>
+  import("@/components/home/HomePageCarousel/ProductCarousel/PopularCarousel")
 );
-const AttractivePlannerCarousel = dynamic(
-  () => import("@/components/home/HomePageCarousel/AttractivePlannerCarousel"),
-  { ssr: false }
+const MultiImagerBannerCarousel = lazy(() =>
+  import(
+    "@/components/home/HomePageCarousel/BannerCarousel/MultiImagerBannerCarousel"
+  )
 );
-const AboutUsSection = dynamic(
-  () => import("@/components/home/AboutUsSection"),
-  { ssr: false }
+const BannerCarousel = lazy(() =>
+  import("@/components/home/HomePageCarousel/BannerCarousel/BannerCarousel")
 );
-const PopularCarousel = dynamic(
-  () =>
-    import(
-      "@/components/home/HomePageCarousel/ProductCarousel/PopularCarousel"
-    ),
-  { ssr: false }
+const InternationDestinationSection = lazy(() =>
+  import("@/components/home/InternationDestinationSection")
 );
-const MultiImagerBannerCarousel = dynamic(
-  () =>
-    import(
-      "@/components/home/HomePageCarousel/BannerCarousel/MultiImagerBannerCarousel"
-    ),
-  { ssr: false }
+const DomesticDestinationSection = lazy(() =>
+  import("@/components/home/DomesticDestination")
 );
-const BannerCarousel = dynamic(
-  () =>
-    import("@/components/home/HomePageCarousel/BannerCarousel/BannerCarousel"),
-  { ssr: false }
+const PopularDestinationSection = lazy(() =>
+  import("@/components/home/PopularDestinationSection")
 );
-const InternationDestinationSection = dynamic(
-  () => import("@/components/home/InternationDestinationSection"),
-  { ssr: false }
-);
-const PopularDestinationSection = dynamic(
-  () => import("@/components/home/PopularDestinationSection"),
-  { ssr: false }
-);
-const DomesticDestinationSection = dynamic(
-  () => import("@/components/home/DomesticDestination"),
-  { ssr: false }
-);
-const OurResorts = dynamic(() => import("@/components/home/OurResorts"), {
-  ssr: false,
-});
-const OurServices = dynamic(() => import("@/components/home/OurServices"), {
-  ssr: false,
-});
-const Footer = dynamic(() => import("@/components/global/Footer"), {
-  ssr: false,
-});
+const OurResorts = lazy(() => import("@/components/home/OurResorts"));
+const OurServices = lazy(() => import("@/components/home/OurServices"));
+const Footer = lazy(() => import("@/components/global/Footer"));
 
 export default function Home() {
   return (
@@ -153,21 +168,67 @@ export default function Home() {
       <WeddingBannerCarousel />
       <HeroSection />
       <HeroSectionCarousel />
-      <TrendingCarousel />
-      <BestSellingCarousel />
-      <DomesticCarousel />
-      <InternationalCarousel />
-      <AttractivePlannerCarousel />
-      <AboutUsSection />
-      <PopularCarousel />
-      <MultiImagerBannerCarousel />
-      <BannerCarousel />
-      <InternationDestinationSection />
-      <PopularDestinationSection />
-      <DomesticDestinationSection />
-      <OurResorts />
-      <OurServices />
-      <Footer />
+      <InViewComponent>
+        <TrendingCarousel />
+      </InViewComponent>
+      <InViewComponent>
+        <BestSellingCarousel />
+      </InViewComponent>
+      <InViewComponent>
+        <DomesticCarousel />
+      </InViewComponent>
+      <InViewComponent>
+        <InternationalCarousel />
+      </InViewComponent>
+      <InViewComponent>
+        <AttractivePlannerCarousel />
+      </InViewComponent>
+      <InViewComponent>
+        <AboutUsSection />
+      </InViewComponent>
+      <InViewComponent>
+        <PopularCarousel />
+      </InViewComponent>
+      <InViewComponent>
+        <MultiImagerBannerCarousel />
+      </InViewComponent>
+      <InViewComponent>
+        <BannerCarousel />
+      </InViewComponent>
+      <InViewComponent>
+        <InternationDestinationSection />
+      </InViewComponent>
+      <InViewComponent>
+        <PopularDestinationSection />
+      </InViewComponent>
+      <InViewComponent>
+        <DomesticDestinationSection />
+      </InViewComponent>
+      <InViewComponent>
+        <OurResorts />
+      </InViewComponent>
+      <InViewComponent>
+        <OurServices />
+      </InViewComponent>
+      <InViewComponent>
+        <Footer />
+      </InViewComponent>
     </>
   );
 }
+
+const InViewComponent = ({ children }) => {
+  return (
+    <InView triggerOnce>
+      {({ inView, ref }) => (
+        <div ref={ref}>
+          {inView ? (
+            <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+          ) : (
+            <div style={{ minHeight: "1px" }} /> // Empty div with minimum height
+          )}
+        </div>
+      )}
+    </InView>
+  );
+};
