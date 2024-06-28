86b324e9ce6a6d9ce7c181c6a3598c135f36cdb7
Naman Rajput <namanrajput801@gmail.com>
2024-06-28 11:44:00 +0530

    ui:changes the style of the pagination button

--- src/components/home/Blog/PaginationButton.jsx
+++ src/components/home/Blog/PaginationButton.jsx
@@ -6,12 +6,8 @@ const PaginationButton = ({ onClick, disabled, children, active }) => {
       onClick={onClick}
       disabled={disabled}
       className={`px-4 py-2 rounded-lg ${
-        disabled
-          ? "bg-black cursor-not-allowed"
-          : active
-          ? "bg-red-600 text-white"
-          : "bg-black"
-      } text-white transition duration-300`}
+        disabled ? " cursor-not-allowed" : active ? "bg-red-600 text-white" : ""
+      } text-black font-semibold transition duration-300`}
     >
       {children}
     </button>
