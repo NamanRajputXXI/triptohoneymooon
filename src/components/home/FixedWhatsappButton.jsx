3c0b48946c496a970e764b6979945439125606da
Naman Rajput <namanrajput801@gmail.com>
2024-07-09 11:25:12 +0530

    ui:make the new popup on whatsapp fixed button

--- src/components/home/FixedWhatsappButton.jsx
+++ src/components/home/FixedWhatsappButton.jsx
@@ -1,20 +1,35 @@
-// @/components/global/FixedWhatsAppButton.js
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 
 const FixedWhatsAppButton = () => {
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    const hasClicked = sessionStorage.getItem("whatsappClicked");
+    if (!hasClicked) {
+      setShowNotification(true);
+    }
+  }, []);
+
   const openWhatsApp = () => {
     window.open("https://wa.me/7303522478", "_blank");
+    setShowNotification(false);
+    sessionStorage.setItem("whatsappClicked", "true");
   };
 
   return (
-    <button
+    <div
       onClick={openWhatsApp}
-      className="fixed left-4 bottom-4 z-50 p-4 rounded-full bg-[#25D366] text-white shadow-lg hover:bg-[#128C7E] transition-colors duration-300"
+      className="fixed left-4 bottom-4 z-50 cursor-pointer p-4 rounded-full bg-[#25D366] text-white transition-colors duration-300"
       aria-label="Contact us on WhatsApp"
     >
+      {showNotification && (
+        <div className="h-5 w-5 -top-1 right-0 flex items-center justify-center rounded-full absolute bg-red-600 text-white">
+          1
+        </div>
+      )}
       <FaWhatsapp size={24} />
-    </button>
+    </div>
   );
 };
 
